a7118789abf3de29449c2d455e69e3e1f5965ad3
suarez12138 <60814832+suarez12138@users.noreply.github.com>
2021-02-03 23:17:14 +0800

    2.3

--- maxProfit5.cpp
+++ maxProfit5.cpp
@@ -13,37 +13,49 @@ class Solution {
 public:
     int maxProfit5(vector<int> &prices) {
         int n = prices.size();
-        if (n == 1 || n == 0) return 0;
-        if (n == 2) return max(0, prices[1] - prices[0]);
-        int result = 0;
-        int buy;
-        bool cold = false, aftercold = false;
-        if (prices[0] < prices[1])
-            buy = prices[0];
-
-
-        for (int i = 1; i < n - 1; ++i) {
-            if (!aftercold) {
-                if (prices[i - 1] < prices[i] && prices[i] >= prices[i + 1]) {
-                    result += prices[i] - buy;
-                    cold = true;
-                    continue;
-                } else if (prices[i - 1] >= prices[i] && prices[i] < prices[i + 1]) {
-                    if (cold) {
-                        aftercold = true;
-                        continue;
-                    }
-                    buy = prices[i];
-
-                }
-            } else if (prices[i - 1] <= prices[i] && prices[i] < prices[i + 1]) buy = prices[i];
-
-
-            cold = false;
-            aftercold = false;
+        int dp_i0 = 0, dp_i1 = INT32_MIN;
+        int before = 0;
+        for (int i = 0; i < n; ++i) {
+            int tmp = dp_i0;
+            dp_i0 = max(dp_i0, dp_i1 + prices[i]);
+            dp_i1 = max(dp_i1, before - prices[i]);
+            before = tmp;
         }
-        if (prices[n - 2] < prices[n - 1])result += prices[n - 1] - buy;
-        return result;
+        return dp_i0;
+
+
+//        int n = prices.size();
+//        if (n == 1 || n == 0) return 0;
+//        if (n == 2) return max(0, prices[1] - prices[0]);
+//        int result = 0;
+//        int buy;
+//        bool cold = false, aftercold = false;
+//        if (prices[0] < prices[1])
+//            buy = prices[0];
+//
+//
+//        for (int i = 1; i < n - 1; ++i) {
+//            if (!aftercold) {
+//                if (prices[i - 1] < prices[i] && prices[i] >= prices[i + 1]) {
+//                    result += prices[i] - buy;
+//                    cold = true;
+//                    continue;
+//                } else if (prices[i - 1] >= prices[i] && prices[i] < prices[i + 1]) {
+//                    if (cold) {
+//                        aftercold = true;
+//                        continue;
+//                    }
+//                    buy = prices[i];
+//
+//                }
+//            } else if (prices[i - 1] <= prices[i] && prices[i] < prices[i + 1]) buy = prices[i];
+//
+//
+//            cold = false;
+//            aftercold = false;
+//        }
+//        if (prices[n - 2] < prices[n - 1])result += prices[n - 1] - buy;
+//        return result;
     }
 };
 
---
+++ maxProfit6.cpp
@@ -0,0 +1,30 @@
+//
+// Created by 99681 on 2021/2/3.
+//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit6(vector<int> &prices, int fee) {
+        int n = prices.size();
+        int dp_i0 = 0, dp_i1 = - prices[0];
+        for (int i = 1; i < n; ++i) {
+            int tmp = dp_i0;
+            dp_i0 = max(dp_i0, dp_i1 + prices[i] - fee);
+            dp_i1 = max(dp_i1, tmp - prices[i]);
+        }
+        return dp_i0;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums{1, 3, 2, 8, 4, 9};
+    int outcome = solution.maxProfit6(nums, 2);
+    cout << outcome << endl;
+    return 0;
+}
+
