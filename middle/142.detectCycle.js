82c93e878a2275f3fc4f2eee2123904db8d6a323
suarez12138 <60814832+suarez12138@users.noreply.github.com>
2021-03-08 17:46:55 +0800

    3.8

--- middle/142.detectCycle.js
+++ middle/142.detectCycle.js
@@ -1,6 +1,27 @@
-a();
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
 
-
-function  a(){
-    console.log("here")
-}
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let fast=head,slow=head;
+    while(fast&&fast.next){
+        fast=fast.next.next;
+        slow=slow.next;
+        if(fast==slow){
+            while(head!=slow){
+                head=head.next;
+                slow=slow.next;
+            }
+            return slow;
+        }
+    }
+    return null;
+};
