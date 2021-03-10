36b33f9444e05fc1f00fa002d152195e6742a5a3
suarez12138 <60814832+suarez12138@users.noreply.github.com>
2021-03-10 12:37:58 +0800

    3.10

---
+++ true/判断URL.js
@@ -0,0 +1,9 @@
+// 写一个函数判断是否属于腾讯系的 URL
+
+function judge(url){
+    let regex=/^(http|https):\/\/(\w+\.)+qq.com\//
+    return regex.test(url)
+}
+
+console.log(judge("https://y.qq.com/?ADTAG=myqq#type=index"))
+console.log(judge("https://mail.qq.com/"))
