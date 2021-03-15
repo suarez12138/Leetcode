c394568b550a7dd742a079b999250f074b26d65c
suarez12138 <60814832+suarez12138@users.noreply.github.com>
2021-03-15 09:30:01 +0800

    3.15

---
+++ true/大数相加.js
@@ -0,0 +1,17 @@
+function add(s1,s2){
+    let n1=s1.length-1,n2=s2.length-1,carry=0;
+    let res=[];
+    while (n1>=0&&n2>=0){
+        let t1=0,t2=0;
+        if (n1>=0)t1=s1.charAt(n1--)-'0';
+        if (n2>=0)t2=s2.charAt(n2--)-'0';
+        res.unshift((t1+t2+carry)%10);
+        carry=Math.floor((t1+t2+carry)/10);
+
+    }
+
+    if (carry)res.unshift(carry);
+    return res.join('')
+}
+
+console.log(add("1", "9"))
