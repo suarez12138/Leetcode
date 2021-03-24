b73ef876de7c7f2fa1d64fb954011f58b6eefebd
suarez12138 <60814832+suarez12138@users.noreply.github.com>
2021-03-24 11:09:46 +0800

    3.24

--- true/堆排序.js
+++ true/堆排序.js
@@ -1,10 +1,10 @@
 function heapsort(arr) {
     let n = arr.length;
-    for (let i = n >> 1 - 1; i >= 0; i--) {
+    for (let i = (n >> 1) - 1; i >= 0; i--) {
         adjust(arr, i, n);
     }
     for (let i = n - 1; i >= 0; i--) {
-        swap(arr, 0, i);
+        [arr[i], arr[0]] = [arr[0], arr[i]];
         adjust(arr, 0, i);
     }
     return arr;
@@ -13,19 +13,15 @@ function heapsort(arr) {
 function adjust(arr, i, len) {
     let tmp = arr[i];
     for (let j = 2 * i + 1; j < len; j = 2 * j + 1) {
-        if (j+1<len&&arr[j]<arr[j+1])j++;
-        if (arr[j]>tmp){
-            swap(arr, i, j);
-            i=j;
-        }else break;
+        if (j + 1 < len && arr[j] < arr[j + 1]) j++;
+        if (arr[j] > tmp) {
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+            i = j;
+        } else break;
     }
 }
 
-function swap(arr,i,j){
-    [arr[i],arr[j]]=[arr[j],arr[i]];
-}
 
 let arr = [3, 2, 5, 3, 4, 6, 4, 2, 1, 6, 4];
 
 console.log(heapsort(arr));
-console.log(arr);
